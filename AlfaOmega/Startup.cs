c6edd84fb98895d3c6da068c1064c765e98dfe3e
Jesus Zamora Román <jgpe2000@gmail.com>
2019-08-10 16:26:05 -0500

    Inicio Proyecto

---
+++ AlfaOmega/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AlfaOmega.Startup))]
+namespace AlfaOmega
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
